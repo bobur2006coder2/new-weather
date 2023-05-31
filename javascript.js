e68fc0d674c1f88fe9f8491af144faea572eaeb1
bobur2006coder <boburdilmurodov702@gmail.com>
2023-05-31 18:42:42 +0500

    fix

--- javascript.js
+++ javascript.js
@@ -1,27 +1,25 @@
 let inp = document.querySelector('.inp')
 let btn=document.querySelector('.btn_for_ipn')
-
-// btn.addEventListener('click')
+let cityName=document.querySelector('.this_city')
+inp.placeholder='Enter the name of the city'
 
 btn.addEventListener('click',(e)=>{
-    e.preventDefault()
     let key = "84a3781201a7f9a9bda21e4f05b6c2f0"
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=${key}`)
-    .then((res) =>res.json())
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp.value}&units=metric&appid=${key}`)
+    .then(res=>res.json())
     .then(data=>{
-    //    let city = document.querySelector(".this_city")
-    //    let gradus = document.querySelector(".for_gradus")
-    //    let temp = Math.floor(data.main.temp)
-    //    gradus.innerHTML = temp + " °C"
-    //    city.textContent = `${data.name}`
-    //    inp.value = ""
-       console.log(data); 
-    
-    }
-    
-    )
+        let gradus=document.querySelector('.for_gradus')
+        cityName.textContent=data.name 
+        gradus.textContent=Math.floor(data.main.temp )+' °C'
+        console.log(data.name);
+        inp.value=''
+    })
+    .catch(err=>{
+        alert('Siz xato bo`lgan mamlakat nomini kiritdingiz');
+        inp.value=''
+        cityName.textContent='Tashkent'
+        
 
+    })
 })
 
-
- 
